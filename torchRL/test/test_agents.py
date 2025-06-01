6a929cea2f5f8c744cd932c14abd7f0749adecdb
Attila Suto <attila.suto@aimotive.com>
2025-06-01 15:11:39 +0200

    Unit tests and updated README.md
    Change-Id: I3d93c865ff5fe4e881f725beedcef6e9147d72c1

---
+++ torchRL/test/test_agents.py
@@ -0,0 +1,13 @@
+import unittest
+from src.agents.ppo_agent import PPOAgent
+
+class TestPPOAgent(unittest.TestCase):
+    def test_agent_initialization(self):
+        agent = PPOAgent()
+        self.assertIsNotNone(agent)
+
+    def test_select_action(self):
+        agent = PPOAgent()
+        dummy_state = [0.0] * agent.state_dim  # Assumes agent has 'state_dim'
+        action = agent.select_action(dummy_state)
+        self.assertIsNotNone(action)
---
+++ torchRL/test/test_env.py
@@ -0,0 +1,9 @@
+import unittest
+from src.envs.env import make_env
+
+class TestEnvironment(unittest.TestCase):
+    def test_make_env(self):
+        env = make_env()
+        self.assertIsNotNone(env)
+        self.assertTrue(hasattr(env, 'reset'))
+        self.assertTrue(hasattr(env, 'step'))
